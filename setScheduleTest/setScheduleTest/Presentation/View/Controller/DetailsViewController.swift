cf9becc07835feabadeffaf0e287bb52ae5f49b5
tazimete <tazimete@gmail.com>
2021-11-01 16:28:29 +0600

    Added Moview details view controller

---
+++ setScheduleTest/setScheduleTest/Presentation/View/Controller/DetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsViewController.swift
+//  setScheduleTest
+//
+//  Created by JMC on 1/11/21.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class DetailsViewController: BaseViewController {
+    public var searchViewModel: AbstractSearchViewModel!
+    private let disposeBag = DisposeBag()
+    private let searchTrigger = PublishSubject<Void>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func initView() {
+        super.initView()
+    }
+    
+    override func initNavigationBar() {
+        self.navigationItem.title = "Details"
+    }
+    
+    override func bindViewModel() {
+        let searchInput = SearchViewModel.SearchInput(searchItemListTrigger: searchTrigger)
+        let searchOutput = searchViewModel.getSearchOutput(input: searchInput)
+    }
+}
