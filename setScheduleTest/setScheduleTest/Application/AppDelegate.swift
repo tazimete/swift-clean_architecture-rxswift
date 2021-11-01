11d6ded7c0c776282927f87d51dfb3ef67febe77
tazimete <tazimete@gmail.com>
2021-11-01 15:35:49 +0600

    Removed unnecessary codebase

--- setScheduleTest/setScheduleTest/Application/AppDelegate.swift
+++ setScheduleTest/setScheduleTest/Application/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        IQKeyboardManager.shared.enable = true
         
         //build app configuration
         buildAppConfig()
